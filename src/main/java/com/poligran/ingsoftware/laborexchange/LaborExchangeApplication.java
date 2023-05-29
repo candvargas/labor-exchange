1dc757da6f82d335fd2646e8d90612f53dd7e76a
candvargas <candvargas@poligran.edu.co>
2023-05-29 13:32:57 -0500

    git init

---
+++ src/main/java/com/poligran/ingsoftware/laborexchange/LaborExchangeApplication.java
@@ -0,0 +1,13 @@
+package com.poligran.ingsoftware.laborexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaborExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaborExchangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poligran/ingsoftware/laborexchange/LaborExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poligran.ingsoftware.laborexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaborExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
