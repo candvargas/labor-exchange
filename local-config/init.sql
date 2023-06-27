a7ac75a848efc0cbf8238de58f97a4598c9136ea
CRISTHIAN ANDRES VARGAS CUERO <candvargas@poligran.edu.co>
2023-06-27 16:07:26 +0000

    Updated init.sql

--- local-config/init.sql
+++ local-config/init.sql
@@ -1,52 +1,57 @@
-CREATE SCHEMA "labor-exchange" AUTHORIZATION postgres;
+CREATE SCHEMA "labor-exchange" AUTHORIZATION root;
 
 CREATE TABLE "labor-exchange".department (
-	id serial2 NOT NULL,
-	"name" varchar NULL,
-	CONSTRAINT department_pk PRIMARY KEY (id)
+    id smallserial NOT NULL,
+    "name" varchar NOT NULL,
+    CONSTRAINT department_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE "labor-exchange".cities (
-	id serial4 NOT NULL,
-	"name" varchar NOT NULL,
-	department_id varchar NULL,
-	CONSTRAINT cities_pk PRIMARY KEY (id)
+    id serial4 NOT NULL,
+    "name" varchar NOT NULL,
+    department_id int2 NULL,
+    CONSTRAINT cities_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE "labor-exchange".offers_types (
-	id serial NOT NULL,
-	"name" varchar NOT NULL,
-	CONSTRAINT types_offers_pk PRIMARY KEY (id)
+    id serial4 NOT NULL,
+    "name" varchar NOT NULL,
+    CONSTRAINT types_offers_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE "labor-exchange".contracts_types (
-	id serial2 NOT NULL,
-	"name" varchar NOT NULL,
-	CONSTRAINT types_contracts_pk PRIMARY KEY (id)
+    id smallserial NOT NULL,
+    "name" varchar NOT NULL,
+    CONSTRAINT types_contracts_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE "labor-exchange".offers (
-	id serial8 NOT NULL,
-	"name" varchar NOT NULL,
-	description text NOT NULL,
-	city_id int4 NULL,
-	salary int4 NULL,
-    offer_type int2 NULL,
-    contract_type int2 NULL,
-	created_at date NOT NULL DEFAULT current_date,
-	CONSTRAINT offers_pk PRIMARY KEY (id)
+    id bigserial NOT NULL,
+    "name" varchar NOT NULL,
+    description text NOT NULL,
+    city_id int4 NOT NULL,
+    salary int4 NOT NULL,
+    offer_type int2 NOT NULL,
+    contract_type int2 NOT NULL,
+    created_at date NOT NULL DEFAULT CURRENT_DATE,
+    active bool NOT NULL DEFAULT true,
+    CONSTRAINT offers_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE "labor-exchange".requirements (
-	id serial NOT NULL,
-	offer_id int8 NOT NULL,
-	education_id int2 NULL,
-	years_of_experience int2 NULL,
-	additional_description text NULL
+    id serial4 NOT NULL,
+    offer_id int8 NOT NULL,
+    education_level_id int2 NULL,
+    years_of_experience int2 NULL,
+    additional_description text NULL,
+    CONSTRAINT requirements_pk PRIMARY KEY (id)
 );
 
+ALTER TABLE "labor-exchange".cities ADD CONSTRAINT cities_fk FOREIGN KEY (department_id) REFERENCES "labor-exchange".department(id);
+ALTER TABLE "labor-exchange".offers ADD CONSTRAINT offers_fk FOREIGN KEY (city_id) REFERENCES "labor-exchange".cities(id);
+ALTER TABLE "labor-exchange".offers ADD CONSTRAINT offers_fk_1 FOREIGN KEY (offer_type_id) REFERENCES "labor-exchange".offers_types(id);
+ALTER TABLE "labor-exchange".offers ADD CONSTRAINT offers_fk_2 FOREIGN KEY (contract_type_id) REFERENCES "labor-exchange".contracts_types(id);
+ALTER TABLE "labor-exchange".requirements ADD CONSTRAINT requirements_fk FOREIGN KEY (offer_id) REFERENCES "labor-exchange".offers(id);
+ALTER TABLE "labor-exchange".requirements ADD CONSTRAINT requirements_fk_1 FOREIGN KEY (education_level_id) REFERENCES "labor-exchange".education_level(id);
 
-INSERT INTO "labor-exchange".offers ("name", description, city_id) VALUES('liniero', 'debe hechar lineas', 1, 1300000,
-1, 1);
-INSERT INTO "labor-exchange".offers ("name", description, city_id) VALUES('excavador', 'debe hechar lineas', 1, 1500000,
- 1, 1);
+
